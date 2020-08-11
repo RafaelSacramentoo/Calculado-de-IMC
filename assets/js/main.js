d146f9043ee58c1dfaf63ff7081e165c7d3b0dd2
RafaelSacramentoo <RafaelSacramentoo@users.noreply.github.com>
2020-08-11 16:45:44 -0300

    Calculadora quase completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,30 +19,31 @@ form.addEventListener('submit', function (e ) {
     }
 
     const imc =getImc(peso,altura);
+    const nivelImc = getnivelImc(imc)
 
+
+    const msg = `Seu IMC é ${imc} (${nivelIMC}).`;
+    setResultado(msg , true);
 
 });
 
-function getnivelImc() {
+function getnivelImc(imc) {
     const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-    'Obesidade grau 1','Obesidade grau 2','Obesidade grau 3']
+    'Obesidade grau 1','Obesidade grau 2','Obesidade grau 3'];
 
-    if(imc>+ 39.9) {
-
-    } else if(imc + 34.9) {
-
-    } else if(imc + 29.9) {
-
-    } else if(imc + 24.9) {
-
-    } else if(imc + 18.5) {
-
-    }
+    if(imc>= 39.9)  return nivel[5];
+    if(imc >= 34.9) return nivel[4];
+    if(imc >= 29.9) return nivel[3];
+    if(imc >= 24.9) return nivel[2];
+    if(imc >= 18.5) return nivel[1];
+    if(imc < 18.5)  return nivel[0];
+    
 }
 
+
 function getImc(peso,altura) {
-    const imc = peso/altura ** 2
-    return imc.toFixed(2)
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
 }
 
 
@@ -56,6 +57,15 @@ function setResultado(msg,isValid) {
     resultado.innerHTML = '';
 
     const p = criaP();
+
+    if (isValid){ 
+        p.classList.add('paragrafo-resultado');
+    } else{
+        p.classList.add('bad');
+    }
+
+
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 
